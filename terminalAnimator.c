3abe6fce9dcb8852f2832b737a01bf325ca7c5ec
Home Desktop-WSL Ubuntu <26182964+ballooner@users.noreply.github.com>
2025-05-11 09:37:12 -0400

    Added function to get size of terminal using VT100 commands

--- terminalAnimator.c
+++ terminalAnimator.c
@@ -3,6 +3,7 @@
 #include <termios.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 
 //Defines
@@ -13,8 +14,8 @@
 struct terminalInfo {
     struct termios originalParams;
     struct termios currParams;
-    int rowNum;
-    int colNum;
+    int rowSize;
+    int colSize;
 };
 
 struct terminalInfo termInfo;
@@ -24,6 +25,7 @@ struct terminalInfo termInfo;
 void kill(const char *s)
 {
     perror(s);
+    exit(-1);
 }
 
 void leaveRawMode()
@@ -38,7 +40,8 @@ void enterRawMode()
 
     atexit(leaveRawMode);
 
-    termInfo.currParams.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | ICRNL | IGNCR | ICRNL | IXON);
+    termInfo.currParams.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | ICRNL
+                                     | IGNCR | ICRNL | IXON);
     termInfo.currParams.c_oflag &= ~OPOST;
     termInfo.currParams.c_lflag &= ~ (ECHO | ECHONL | ICANON | ISIG | IEXTEN);
     termInfo.currParams.c_cflag &= ~(CSIZE | PARENB);
@@ -47,15 +50,38 @@ void enterRawMode()
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &termInfo.currParams);
 }
 
+int getTerminalSize(int *row, int *col)
+{
+    if (write(STDIN_FILENO, "\x1b[999B\x1b[999C\x1b[6n", 16) == -1) return -1;
+
+    printf("\r\n");
+    char buffer[32];
+    unsigned int index = 0;
+
+    while (read(STDIN_FILENO, &buffer[index], 1) == 1)
+    {
+        if (index > sizeof(buffer) - 1) break;
+        if (buffer[index++] == 'R') break;
+    }
+
+    buffer[index] = '\0';
+
+    if (buffer[0] != '\x1b' && buffer[1] != '[') return -1;
+    sscanf(&buffer[2], "%d;%d", row, col);
+
+    return 0;
+}
 
 //Input
-
 
 
 //Init
 int main(void)
 {
     enterRawMode();
+    if (getTerminalSize(&termInfo.rowSize, &termInfo.colSize) == -1) 
+        kill("getTerminalSize");
+    printf("row: %d, col: %d\r\n", termInfo.rowSize, termInfo.colSize);
 
     char c;
 
